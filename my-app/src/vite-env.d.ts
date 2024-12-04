080a0e0b7a7bea470f04cbc3b384d68befb7656e
Pavel Gordienko <guzmo@yandex.ru>
2024-12-04 23:04:33 +0300

    feat: init empty react

---
+++ my-app/src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+declare module 'vite-plugin-eslint' {
+  import { Plugin } from 'vite';
+  const eslintPlugin: () => Plugin;
+  export default eslintPlugin;
+}
