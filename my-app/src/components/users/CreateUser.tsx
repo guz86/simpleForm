a747d979c6d3e64cf68b787a6badc87d68d568b8
Pavel Gordienko <guzmo@yandex.ru>
2024-12-05 11:12:20 +0300

    feat: add state

--- my-app/src/components/users/CreateUser.tsx
+++ my-app/src/components/users/CreateUser.tsx
@@ -1,21 +1,49 @@
-import { FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Card } from '../ui/Card';
 import styles from './CreateUser.module.css';
 import { Button } from '../ui/Button';
 
 export const CreateUser = () => {
+  const [inputName, setInputName] = useState('');
+  const [inputAge, setInputAge] = useState<number | ''>('');
+
   const createUserHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (inputName.trim().length === 0) {
+      return;
+    }
+
+    console.log(inputName, inputAge);
+    setInputName('');
+    setInputAge('');
   };
+
+  function nameChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    setInputName(event.target.value);
+  }
+  function ageChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    setInputAge(event.target.value === '' ? '' : +event.target.value);
+  }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={createUserHandler}>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          onChange={nameChangeHandler}
+          value={inputName}
+        />
 
         <label htmlFor="age">Age</label>
-        <input id="age" type="age" />
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={inputAge}
+          min="1"
+        />
 
         <Button type="submit">Add user</Button>
       </form>
