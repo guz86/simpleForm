fac3aea8c2baa363816ff6fb4155ed635badb62c
Pavel Gordienko <guzmo@yandex.ru>
2024-12-05 15:21:48 +0300

    feat: use portals

--- my-app/src/components/ui/ErrorModal.tsx
+++ my-app/src/components/ui/ErrorModal.tsx
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import { Button } from './Button';
 import { Card } from './Card';
 import styles from './ErrorModal.module.css';
@@ -8,25 +9,56 @@ interface ErrorModalProps {
   onCloseModal: () => void;
 }
 
+const Backdrop = ({ onClose }: { onClose: () => void }) => {
+  return <div className={styles.backdrop} onClick={onClose}></div>;
+};
+
+const Modal = ({
+  title,
+  message,
+  onClose,
+}: {
+  title: string;
+  message: string;
+  onClose: () => void;
+}) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
 export const ErrorModal: React.FC<ErrorModalProps> = ({
   title,
   message,
   onCloseModal,
 }) => {
+  const backdropRoot = document.getElementById('backdrop');
+  const modalRoot = document.getElementById('modal');
+
+  if (!backdropRoot || !modalRoot) {
+    console.error(
+      'No elements with ID backdrop or modal in the DOM were found.'
+    );
+    return null;
+  }
+
   return (
     <>
-      <div className={styles.backdrop} onClick={onCloseModal}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onCloseModal}>Close</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(<Backdrop onClose={onCloseModal} />, backdropRoot)}
+      {ReactDOM.createPortal(
+        <Modal title={title} message={message} onClose={onCloseModal} />,
+        modalRoot
+      )}
     </>
   );
 };
